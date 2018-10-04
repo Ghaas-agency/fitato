5a28c69106d3a8ad823dfc470a3d25974e1081e6
Akshay Dhiman <akshay.virgo@gmail.com>
2018-10-04 12:52:37 +0530

    added express-handlebars, firestore db, updated firebase.json rules

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,28 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const express = require('express');
+const hbs = require('express-handlebars');
+admin.initializeApp(functions.config().firebase);
 
 const app = express();
 
-app.get('/test', (req, res) => {
-  res.send('Hello');
-})
+const db = admin.firestore();
 
-exports.helloWorld = functions.https.onRequest(app);
+db.collection('activities').get()
+  .then(snap => {
+    snap.forEach(doc => {
+      /* console.log(doc.id); */
+    });
+  })
+  .catch(err => console.log(err));
+
+
+// Handlebars middleware.
+app.engine('handlebars', hbs({defaultLayout: 'index'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+exports.app = functions.https.onRequest(app);
