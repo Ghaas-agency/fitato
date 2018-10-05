f5dfde0f7abd5440f21459d32038d5d65d5e586f
Akshay Dhiman <akshay.virgo@gmail.com>
2018-10-05 18:59:57 +0530

    added 404 handling

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
+const path = require('path');
 const hbs = require('express-handlebars');
 admin.initializeApp(functions.config().firebase);
 
@@ -18,6 +19,7 @@ async function getOptions() {
       snap.forEach(doc => {
         arr.push({id: doc.id, text: doc.data().text});
       });
+      arr.unshift({id: 'all', text: 'All'});
       return arr;
     })
     .catch(err => console.log(err));
@@ -28,6 +30,7 @@ async function getOptions() {
       snap.forEach(doc => {
         arr.push({id: doc.id, text: doc.data().text});
       });
+      arr.unshift({id: 'all', text: 'All'});
       return arr;
     })
     .catch(err => console.log(err));
@@ -45,6 +48,26 @@ app.get('/partner', async (req, res) => {
   res.render('home', {
     data: await getOptions()
   });
+});
+
+// Handle 404 error
+app.use((req, res, next) => {
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
 });
 
 exports.app = functions.https.onRequest(app);
