cc74852f8d4cbd74fb5327c13d3bd1520cc4d313
Akshay Dhiman <akshay.virgo@gmail.com>
2018-10-28 19:45:53 +0530

    functions: replaced handlebars view engine with pug

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,18 @@
 const functions = require('firebase-functions');
 const express = require('express');
-const hbs = require('express-handlebars');
+
+const path = require('path');
 const basicAuth = require('express-basic-auth');
 
 const app = express();
 
+// Controllers
 const api = require('./controller/api');
 const client = require('./controller/client');
 
-// Handlebars middleware.
-app.engine('handlebars', hbs({defaultLayout: 'index'}));
-app.set('view engine', 'handlebars');
+// Pug middleware.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // Routes
 app.use('/api', basicAuth({
