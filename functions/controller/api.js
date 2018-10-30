4389812ec663b71a8f5c44c074964475b70e7da1
Akshay Dhiman <akshay.virgo@gmail.com>
2018-10-30 16:50:17 +0530

    functions: added cache control headers in api routes

--- functions/controller/api.js
+++ functions/controller/api.js
@@ -5,21 +5,25 @@ const router = express.Router();
 const db = require('../model/firestore');
 
 router.get('/options/:city', async (req, res) => {
+  res.set('Cache-Control', 'public, max-age=0, s-maxage=86400');
   res.json(await db.getOptions(req.params.city));
 });
 
 router.get('/activities', async (req, res) => {
+  res.set('Cache-Control', 'public, max-age=0, s-maxage=86400');
   res.json(await db.allActivities());
 });
 
 router.get('/activities/:city', async (req, res) => {
+  res.set('Cache-Control', 'public, max-age=0, s-maxage=86400');
   const response = await db.allActivitiesCitywise(req.params.city);
   res.json((response.length > 0) ? response : {err: 'City not found.'});
 });
 
 router.get('/activities/:city/:activity', async (req, res) => {
-  const { city, activity } = req.params;
+  res.set('Cache-Control', 'public, max-age=0, s-maxage=86400');
 
+  const { city, activity } = req.params;
   const currentAct = await db.checkActivity(activity, city);
 
   // If the activity exists in that city, then send the results as response.
@@ -27,8 +31,9 @@ router.get('/activities/:city/:activity', async (req, res) => {
 });
 
 router.get('/activities/:city/:activity/:location', async (req, res) => {
-  const { city, activity, location } = req.params;
+  res.set('Cache-Control', 'public, max-age=0, s-maxage=86400');
 
+  const { city, activity, location } = req.params;
   const currentAct = await db.checkActivity(activity, city);
 
   // If the activity exists in that city, then send the results as response.
@@ -36,14 +41,18 @@ router.get('/activities/:city/:activity/:location', async (req, res) => {
 });
 
 router.get('/locations', async (req, res) => {
+  res.set('Cache-Control', 'public, max-age=0, s-maxage=86400');
   res.json(await db.allLocations());
 });
 
 router.get('/locations/:city', async (req, res) => {
+  res.set('Cache-Control', 'public, max-age=0, s-maxage=86400');
   res.json(await db.allLocationsCitywise(req.params.city));
 });
 
 router.get('/locations/:city/:location', async (req, res) => {
+  res.set('Cache-Control', 'public, max-age=0, s-maxage=86400');
+  
   const { city, location } = req.params;
 
   const currentLoc = await db.checkLocation(location, city);
