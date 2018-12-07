d6deae4a02b3c47f95918f2561643d59297903d8
Akshay Dhiman <akshay.virgo@gmail.com>
2018-12-07 20:04:59 +0530

    gatsby: /how-it-works updated images

--- src/pages/how-it-works.js
+++ src/pages/how-it-works.js
@@ -49,42 +49,42 @@ class HowItWorksPage extends React.Component {
           <section className="works-steps">
             <div className="container">
               <div className="works-steps__items">
-                <div className="works-steps__item">
+                <div className="works-steps__item--left">
                   <p className="works-steps--step">Step 1</p>
                   <h2>Get The Fitato App</h2>
                   <p>Fitato membership is only accessible with the Fitato app. The apps are available on both Android and iOS.</p>
                   <p>Don’t have the app yet?</p>
                   <Link to="/download/" className="button" style={{marginLeft: '0'}}>Download Now</Link>
                 </div>
-                <div className="works-steps__item works-steps__item--center ">
-                  <img src={withPrefix('/static/index/section-four-1.png')} alt=""/>
+                <div className="works-steps__item--right">
+                  <img src={withPrefix('/static/how-it-works/fitato-how-it-works-step-1.jpg')} alt="how fitato app works download the app"/>
                 </div>
               </div>
               <div className="works-steps__items">
-                <div className="works-steps__item works-steps__item--center ">
-                  <img src={withPrefix('/static/index/section-four-1.png')} alt=""/>
+                <div className="works-steps__item--right">
+                  <img src={withPrefix('/static/how-it-works/fitato-how-it-works-step-2.jpg')} alt="how fitato app works signup"/>
                 </div>
-                <div className="works-steps__item">
+                <div className="works-steps__item--left">
                   <p className="works-steps--step">Step 2</p>
                   <h2>Signup</h2>
                   <p>Once you have the app on your phone, we need to get to know each other a little. Signup for an account using your Email or Facebook account.</p>
                 </div>
               </div>
               <div className="works-steps__items">
-                <div className="works-steps__item">
+                <div className="works-steps__item--left">
                 <p className="works-steps--step">Step 3</p>
                   <h2>Choose Your Preferred Membership</h2>
                   <p>Monthy, Bi-monthly or Quarterly, choose the membership duration that suits you the best. Tap on the Buy Pass button and select your preferred membership style.</p>
                 </div>
-                <div className="works-steps__item works-steps__item--center ">
-                  <img src={withPrefix('/static/index/section-four-2.png')} alt=""/>
+                <div className="works-steps__item--right">
+                  <img src={withPrefix('/static/how-it-works/fitato-how-it-works-step-3.jpg')} alt="how fitato app works choose membership"/>
                 </div>
               </div>
               <div className="works-steps__items">
-                <div className="works-steps__item works-steps__item--center ">
-                  <img src={withPrefix('/static/index/section-four-3.png')} alt=""/>
+                <div className="works-steps__item--right">
+                  <img src={withPrefix('/static/how-it-works/fitato-how-it-works-step-4.jpg')} alt="how fitato app works workout"/>
                 </div>
-                <div className="works-steps__item">
+                <div className="works-steps__item--left">
                 <p className="works-steps--step">Step 4</p>
                   <h2>Get Ready to Sweat</h2>
                   <p>As soon as you purchase your membership, you’d be able to choose and reserve your favourite activity from the list. Gym, Zumba, Yoga, Crossfit, Swimming and a host of options to choose from.</p>
